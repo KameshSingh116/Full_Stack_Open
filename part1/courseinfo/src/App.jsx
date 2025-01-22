45c8613c4f2f8fb853a375d6cf0007582c52126a
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-01-22 15:26:30 +0530

    exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,27 +1,20 @@
-const App = () => {
-  const course = 'Kalilinux'
-  const part1 = 'Basic commands and networking'
-  const exercises1 = 10
-  const part2 = 'Understanding different tools,uses and laws'
-  const exercises2 = 7
-  const part3 = 'Practicing using virtual labs'
-  const exercises3 = 14
-
+const Content = () => {
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Part name="Part 1" exercises={10} />
+      <Part name="Part 2" exercises={7} />
+      <Part name="Part 3" exercises={5} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+const Part = ({ name, exercises }) => {
+  return (
+    <div>
+      <h3>{name}</h3>
+      <p>Number of exercises: {exercises}</p>
+    </div>
+  );
+};
+
+export default Content
