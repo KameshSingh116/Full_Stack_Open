6c45c1e8d45106a7a5a31d79bc938d671fa20e2a
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-01-29 00:10:25 +0530

    exercise 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,13 +1,8 @@
 import { useState } from 'react';
-
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
   const average = total > 0 ? (good - bad) / total : 0;
   const positivePercentage = total > 0 ? (good / total) * 100 : 0;
-
-  if (total === 0) {
-    return <p>No feedback given</p>;
-  }
 
   return (
     <div>
@@ -21,15 +16,16 @@ const Statistics = ({ good, neutral, bad }) => {
   );
 };
 
+
 const App = () => {
+
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
   const handleGood = () => setGood(good + 1);
   const handleNeutral = () => setNeutral(neutral + 1);
   const handleBad = () => setBad(bad + 1);
-
+  const total = good + neutral + bad;
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -38,7 +34,11 @@ const App = () => {
       <button onClick={handleBad}>Bad</button>
 
       <h2>Statistics</h2>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      {total === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      )}
     </div>
   );
 };
