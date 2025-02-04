ffd392e30a98bf9e34439ed73302c32e95b365fc
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-02-04 23:55:47 +0530

    exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-1234567' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
+  ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -13,9 +19,15 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   const addPerson = (event) => {
     event.preventDefault();
-    const personExists = persons.some((person) => person.name === newName);
+    const personExists = persons.some(
+      (person) => person.name.toLowerCase() === newName.toLowerCase()
+    );
     if (personExists) {
       alert(`${newName} is already added to the phonebook`);
     } else {
@@ -26,9 +38,17 @@ const App = () => {
     }
   };
 
+  const personsToShow = persons.filter((person) =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{' '}
+        <input value={searchTerm} onChange={handleSearchChange} />
+      </div>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -42,7 +62,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person, index) => (
+        {personsToShow.map((person, index) => (
           <li key={index}>
             {person.name} {person.number}
           </li>
