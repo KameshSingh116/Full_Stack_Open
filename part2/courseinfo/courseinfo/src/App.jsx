fb054539f36018fc38a44ab91e7db8383774bbde
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-02-04 23:42:44 +0530

    exercise 2.5

--- part2/courseinfo/courseinfo/src/App.jsx
+++ part2/courseinfo/courseinfo/src/App.jsx
@@ -1,92 +1,58 @@
+import Course from './components/Course';
+
 const App = () => {
   const courses = [
     {
-      name: 'Half Stack application development',
       id: 1,
+      name: 'Half Stack application development',
       parts: [
         {
           name: 'Fundamentals of React',
           exercises: 10,
-          id: 1
+          id: 1,
         },
         {
           name: 'Using props to pass data',
           exercises: 7,
-          id: 2
+          id: 2,
         },
         {
           name: 'State of a component',
           exercises: 14,
-          id: 3
+          id: 3,
         },
         {
           name: 'Redux',
           exercises: 11,
-          id: 4
-        }
-      ]
+          id: 4,
+        },
+      ],
     },
     {
-      name: 'Node.js',
       id: 2,
+      name: 'Node.js',
       parts: [
         {
           name: 'Routing',
           exercises: 3,
-          id: 1
+          id: 1,
         },
         {
           name: 'Middlewares',
           exercises: 7,
-          id: 2
-        }
-      ]
-    }
+          id: 2,
+        },
+      ],
+    },
   ];
 
   return (
     <div>
       <h1>Web Development Curriculum</h1>
-      {courses.map(course => (
+      {courses.map((course) => (
         <Course key={course.id} course={course} />
       ))}
     </div>
-  );
-};
-
-const Course = ({ course }) => {
-  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
-
-  return (
-    <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <p>
-        <strong>Total number of exercises: {totalExercises}</strong>
-      </p>
-    </div>
-  );
-};
-
-const Header = ({ course }) => {
-  return <h2>{course.name}</h2>;
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => (
-        <Part key={part.id} part={part} />
-      ))}
-    </div>
-  );
-};
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
   );
 };
 
