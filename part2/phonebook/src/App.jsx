86a2f67d17891b2edb882c7e615cd1998fbbc71c
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-02-05 00:33:31 +0530

    exercise 2.13

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import personService from './services/person';
 
-
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
-  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     personService
@@ -19,109 +17,40 @@ const App = () => {
       });
   }, []);
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
-  };
-
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value);
-  };
-
   const addPerson = (event) => {
     event.preventDefault();
-
-    const personObject = {
-      name: newName,
-      number: newNumber,
-    };
-
-    const existingPerson = persons.find(
-      (person) => person.name === newName
-    );
-
-    if (existingPerson) {
-      if (
-        window.confirm(
-          `${newName} is already added to phonebook, replace the old number with a new one?`
-        )
-      ) {
-        personService
-          .update(existingPerson.id, personObject)
-          .then((response) => {
-            setPersons(
-              persons.map((person) =>
-                person.id !== existingPerson.id ? person : response.data
-              )
-            );
-            setNewName('');
-            setNewNumber('');
-          })
-          .catch((error) => {
-            console.error('Error updating person:', error);
-          });
-      }
-    } else {
-      personService
-        .create(personObject)
-        .then((response) => {
-          setPersons(persons.concat(response.data));
-          setNewName('');
-          setNewNumber('');
-        })
-        .catch((error) => {
-          console.error('Error adding person:', error);
-        });
-    }
+    const personObject = { name: newName, number: newNumber };
+    personService
+      .create(personObject)
+      .then((response) => {
+        setPersons(persons.concat(response.data));
+        setNewName('');
+        setNewNumber('');
+      })
+      .catch((error) => {
+        console.error('Error adding person:', error);
+      });
   };
-
-  const deletePerson = (id) => {
-    if (window.confirm('Are you sure you want to delete this person?')) {
-      personService
-        .remove(id)
-        .then(() => {
-          setPersons(persons.filter((person) => person.id !== id));
-        })
-        .catch((error) => {
-          console.error('Error deleting person:', error);
-        });
-    }
-  };
-
-  const personsToShow = filter
-    ? persons.filter((person) =>
-        person.name.toLowerCase().includes(filter.toLowerCase())
-      )
-    : persons;
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with{' '}
-        <input value={filter} onChange={handleFilterChange} />
-      </div>
-      <h3>Add a new</h3>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+          name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
+          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h3>Numbers</h3>
+      <h2>Numbers</h2>
       <ul>
-        {personsToShow.map((person) => (
+        {persons.map((person) => (
           <li key={person.id}>
-            {person.name} {person.number}{' '}
-            <button onClick={() => deletePerson(person.id)}>delete</button>
+            {person.name} {person.number}
           </li>
         ))}
       </ul>
